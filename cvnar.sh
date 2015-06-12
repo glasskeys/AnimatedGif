99fe735b6cae3a107ecf0938ac00a64593058c34
Stephen Glasskeys <glasskeys@users.noreply.github.com>
2015-06-12 07:42:27 -0400

    Create cvnar.sh
    This script has been tested under bash on Mac OS X -- it should run in Linux too.

---
+++ cvnar.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# DO NOT RUN IN A FOLDER THAT ALREADY CONTAINS JPEG (*.JPG) IMAGES -- THEY WILL BE DELETED!!!
+
+#Make Backup of Nar file
+#echo "Making backup"
+#cp $1 "$1.bak"
+
+#Strip Extension
+tempfile="${1##*/}"
+fname="${tempfile%.*}"
+
+#Unzip the file
+echo "Unzipping NAR file contents"
+unzip $1
+wait ${!}
+
+echo "Resizing Images"
+#Resize Images
+mogrify -resize 320 *.jpg
+
+#Convert JPG to Animated GIF
+convert -delay 15 -loop 0 *.jpg "$fname.gif"
+
+#Clean Up 
+rm -f *.jpg
+rm -f content.xml
+echo "Finished"
