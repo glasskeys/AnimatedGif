7455fbc7cea8b2f3cd4770f518a816796417f82d
Stephen Glasskeys <glasskeys@users.noreply.github.com>
2015-06-12 07:45:23 -0400

    Create cvnar.bat
    This has been tested and works on Windows 8.1. You should probably install 7z -- if you haven't already -- and add the 7z folder to your PATH environment variable.

---
+++ cvnar.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM DO NOT RUN IN A FOLDER THAT ALREADY CONTAINS JPEG (*.JPG) IMAGES OR XML FILES -- THEY WILL BE DELETED!!!
+
+echo Creating zip from NAR file
+copy %1 *.zip
+
+echo Unzipping NAR file contents
+7z e %~n1.zip
+
+echo Resizing images
+mogrify -resize 320 *.jpg
+
+convert -delay 15 -loop 0 *.jpg %~n1.gif
+
+echo Cleaning up
+del *.jpg
+del *.xml
+del %~n1.zip
+echo Finished
+
